7a26310deaf8dda215df8c54a85c092937c6cc3c
danielbuscor <daniel.bus.cor@gmail.com>
2025-08-22 20:39:32 -0400

    Creada la variable amigos y la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let amigos = [];
 
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (nombre !== ""){
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        console.log(amigos);
+    }else {
+        alert("El nombre no puede estar vacío");
+    }
+}
