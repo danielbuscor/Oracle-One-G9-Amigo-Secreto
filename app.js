a80126c9ce5ce60e921164c91a13c142e9159549
danielbuscor <daniel.bus.cor@gmail.com>
2025-08-24 19:12:48 -0400

    Creada la funcion mostrarAmigos e implementada la llamada a dicha funcion dentro de la funcion agregarAmigo

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ function agregarAmigo() {
     }else {
         alert("El nombre no puede estar vacío");
     }
+    mostrarAmigos(); // Se llama a la funcion para actualizar la lista en el HTML con el amigo recien agregado
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");// Se obtiene la referencia a la lista
+    lista.innerHTML = ""; // Limpia la lista antes de agregar algun nuevo elemento
+    amigos.forEach(function(amigo) { // Loop encargado de iterar el array amigos
+        let li = document.createElement("li"); // Crear un nuevo elemento de la lista
+        li.textContent = amigo; // Asignar el nombre del amigo al elemento de la lista
+        lista.appendChild(li); // Con esta linea agregamos el elemento para que aparezca en el HTML
+    });
+
 }
